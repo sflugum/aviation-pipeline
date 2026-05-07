3fb9054f4a38263a32d2a80ad903d729f426e678
Silas Flugum <siclops.ch@gmail.com>
2026-05-07 09:37:35 -0400

    Update main method w/Spring Boot

--- src/main/java/com/aviation/EtlApp.java
+++ src/main/java/com/aviation/EtlApp.java
@@ -1,11 +1,13 @@
 package com.aviation;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class EtlApp {
     public static void main( String[] args )
     {
-        
+        SpringApplication.run(EtlApp.class, args);
 
     }
 }
