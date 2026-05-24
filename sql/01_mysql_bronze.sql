c5d5ea6127e5c2ad54e6b6d681e795fe85ad3e08
Silas Flugum <siclops.ch@gmail.com>
2026-05-23 22:48:58 -0400

    feat: add SQL and Postgres scripts

--- sql/01_mysql_bronze.sql
+++ sql/01_mysql_bronze.sql
@@ -1,1 +1,12 @@
--- TODO DDL scripts
+DROP TABLE IF EXISTS opensky_data_raw;
+
+CREATE TABLE opensky_raw_data (
+    raw_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    raw_data JSON NOT NULL,
+    ingestion_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+
+
